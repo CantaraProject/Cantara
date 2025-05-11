8095e10ffad4c777b013ce57ede0f61b2b080d7e
Jan Martin Reckel <jm.reckel@t-online.de>
2025-05-11 20:55:13 +0200

    Create build.rs file for automatically running up to date, audited 2 packages in 714ms
    found 0 vulnerabilities

---
+++ build.rs
@@ -0,0 +1,24 @@
+//! Cantara's frontend depends on npm packages which this build skript will automatically install if the 'dist/' folder does not exist in the repository.
+
+use std::fs;
+use std::process::Command;
+
+fn main() {
+    // Check if "dist" folder exists
+    if !fs::metadata("node_modules").is_ok() {
+        // Run npm install
+        let output = Command::new("npm")
+            .arg("install")
+            .output()
+            .expect("Failed to execute npm install. Make sure that you have npm installed.");
+
+        // Print output for debugging
+        if !output.status.success() {
+            eprintln!(
+                "npm install failed: {:?}",
+                String::from_utf8_lossy(&output.stderr)
+            );
+            panic!("npm install failed");
+        }
+    }
+}
