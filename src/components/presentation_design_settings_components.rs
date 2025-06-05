76cb279534851984c38fa7a1ad6e017edc255e24
Jan Martin Reckel <jm.reckel@t-online.de>
2025-06-05 20:38:20 +0200

    Add Meta Settings for Presentation Design

--- src/components/presentation_design_settings_components.rs
+++ src/components/presentation_design_settings_components.rs
@@ -12,9 +12,12 @@ rust_i18n::i18n!("locales", fallback = "en");
 
 /// This page contains the general settings for Cantara
 #[component]
-pub fn PresentationDesignSettingsPage(index: u16) -> Element {
+pub fn PresentationDesignSettingsPage(
+    /// The index of the presentation design
+    index: u16,
+    ) -> Element {
     let nav = navigator();
-    let settings = use_settings();
+    let mut settings = use_settings();
 
     let selected_presentation_design_option: Signal<Option<PresentationDesign>> =
         use_signal(|| {
@@ -46,7 +49,15 @@ pub fn PresentationDesignSettingsPage(index: u16) -> Element {
             }
             main {
                 class: "container-fluid content height-100",
-                p { "Some Content "}
+                PresentationDesignMetaSettings {
+                    presentation_design: selected_presentation_design(),
+                    on_pd_changed: move |pd: PresentationDesign| {
+                        let mut settings_write = settings.write();
+                        let mut origin_pd = settings_write.presentation_designs.get_mut(index as usize).unwrap();
+                        origin_pd.name = pd.name;
+                        origin_pd.description = pd.description;
+                    }
+                }
             }
             footer {
                 class: "bottom-bar",
@@ -55,6 +66,47 @@ pub fn PresentationDesignSettingsPage(index: u16) -> Element {
                         nav.replace(crate::Route::SettingsPage {});
                     },
                     { t!("settings.close") }
+                }
+            }
+        }
+    }
+}
+
+#[component]
+pub fn PresentationDesignMetaSettings(
+    /// The presentation design which Meta information shuold be able to be edited
+    presentation_design: PresentationDesign,
+
+    /// A closure which is called each time when the presentation design has been changed
+    on_pd_changed: EventHandler<PresentationDesign>,
+) -> Element {
+
+    let mut pd = use_signal(|| presentation_design);
+
+    rsx! {
+        h3 { "Meta information" }
+        form {
+            fieldset {
+                label {
+                    "Name",
+                    input {
+                        value: pd().name,
+                        onchange: move |event| {
+                            pd.write().name = event.value().clone();
+                            on_pd_changed.call(pd());
+                        }
+                    }
+                }
+
+                label {
+                    "Description",
+                    input {
+                        value: pd().description,
+                        onchange: move |event| {
+                            pd.write().description = event.value().clone();
+                            on_pd_changed.call(pd());
+                        }
+                    }
                 }
             }
         }
