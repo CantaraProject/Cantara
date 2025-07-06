454fb17c8469fe8c937ee5141d5c8c824f58bf36
Jan Martin Reckel <jm.reckel@t-online.de>
2025-07-06 10:55:14 +0200

    Add correct spoiler labeling

--- src/components/font_settings.rs
+++ src/components/font_settings.rs
@@ -32,6 +32,10 @@ pub fn FontRepresentationsComponent(
                         0 => true,
                         _ => false
                     },
+                    is_spoiler: match fonts_count() {
+                        3.. => idx == fonts_count()-2,
+                        _ => idx == fonts_count()-1
+                    },
                     onchange: move |new_font| {
                         match fonts.write().get_mut(idx) {
                             Some(reference) => {
